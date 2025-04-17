810bbc24c29203fc0f5e740bf5d32e374e987afe
KreKBF <es.rusakov.ka@gmail.com>
2025-04-17 20:11:15 +0200

    Update InteractiveDeckSelector.jsx

--- src/InteractiveDeckSelector.jsx
+++ src/InteractiveDeckSelector.jsx
@@ -4,82 +4,80 @@ import "./InteractiveDeckSelector.css";
 export default function InteractiveDeckSelector({ zones, onZoneToggle }) {
   return (
     <svg
-      viewBox="0 0 300 600"
+      viewBox="0 0 400 800"
       xmlns="http://www.w3.org/2000/svg"
       className="deck-schematic"
     >
-      {/* Foredeck (triangle) */}
-      <g onClick={() => onZoneToggle("foredeck")}
-         style={{ cursor: "pointer" }}>
+      {/* Foredeck */}
+      <g onClick={() => onZoneToggle("foredeck")} style={{ cursor: "pointer" }}>
         <polygon
-          points="150,20 190,80 110,80"
+          points="200,40 260,140 140,140"
           fill={zones.foredeck ? "#0077b6" : "#d9d9d9"}
           stroke="#333"
         />
-        <text x="150" y="60" textAnchor="middle" fill="#fff" fontSize="10">
+        <text x="200" y="110" textAnchor="middle" fill="#fff" fontSize="14">
           Foredeck
         </text>
       </g>
 
-      {/* Cockpit (rectangle) */}
-      <g onClick={() => onZoneToggle("cockpit")}
-         style={{ cursor: "pointer" }}>
+      {/* Cockpit */}
+      <g onClick={() => onZoneToggle("cockpit")} style={{ cursor: "pointer" }}>
         <rect
-          x="110"
-          y="80"
-          width="80"
-          height="100"
+          x="140"
+          y="140"
+          width="120"
+          height="140"
           fill={zones.cockpit ? "#0077b6" : "#d9d9d9"}
           stroke="#333"
         />
-        <text x="150" y="130" textAnchor="middle" fill="#fff" fontSize="10">
+        <text x="200" y="210" textAnchor="middle" fill="#fff" fontSize="14">
           Cockpit
         </text>
       </g>
 
-      {/* Flybridge (square inside cockpit) */}
-      <g onClick={() => onZoneToggle("flybridge")}
-         style={{ cursor: "pointer" }}>
+      {/* Flybridge */}
+      <g onClick={() => onZoneToggle("flybridge")} style={{ cursor: "pointer" }}>
         <rect
-          x="130"
-          y="95"
-          width="40"
-          height="40"
+          x="170"
+          y="165"
+          width="60"
+          height="60"
           fill={zones.flybridge ? "#023e8a" : "#bdbdbd"}
           stroke="#333"
         />
-        <text x="150" y="118" textAnchor="middle" fill="#fff" fontSize="8">
+        <text x="200" y="200" textAnchor="middle" fill="#fff" fontSize="10">
           Flybridge
         </text>
       </g>
 
-      {/* Side Decks (trapezoids) */}
-      <g onClick={() => onZoneToggle("sideDecks")}
-         style={{ cursor: "pointer" }}>
+      {/* Side Decks */}
+      <g onClick={() => onZoneToggle("sideDecks")} style={{ cursor: "pointer" }}>
         <polygon
-          points="90,80 105,80 105,300 90,320"
+          points="100,140 140,140 140,480 100,520"
           fill={zones.sideDecks ? "#0077b6" : "#d9d9d9"}
           stroke="#333"
         />
         <polygon
-          points="195,80 190,80 190,300 195,320"
+          points="300,140 260,140 260,480 300,520"
           fill={zones.sideDecks ? "#0077b6" : "#d9d9d9"}
           stroke="#333"
         />
-        <text x="150" y="250" textAnchor="middle" fill="#fff" fontSize="10">
+        <text x="200" y="350" textAnchor="middle" fill="#fff" fontSize="14">
           Side Decks
         </text>
       </g>
 
-      {/* Swim Platform (trapezoid bottom) */}
-      <g onClick={() => onZoneToggle("swimPlatform")}
-         style={{ cursor: "pointer" }}>
+      {/* Swim Platform */}
+      <g
+        onClick={() => onZoneToggle("swimPlatform")}
+        style={{ cursor: "pointer" }}
+      >
         <polygon
-          points="100,320 200,320 180,360 120,360"
+          points="140,520 260,520 240,580 160,580"
           fill={zones.swimPlatform ? "#0077b6" : "#d9d9d9"}
           stroke="#333"
         />
-        <text x="150" y="345" textAnchor="middle" fill="#fff" fontSize="10">
+        <text x="200" y="560" textAnchor="middle" fill="#fff" fontSize="14">
           Swim Platform
         </text>
       </g>
