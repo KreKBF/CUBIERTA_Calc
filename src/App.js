c11bee60ff975d9f574bddb3c6a4646d4a48edc0
KreKBF <es.rusakov.ka@gmail.com>
2025-08-22 02:05:39 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import DeckAreaCalculator from './DeckAreaCalculator';
 import StartForm from './StartForm';
-
+import ThankYou from './ThankYou';
 
 export default function App() {
 return (
@@ -9,6 +9,7 @@ return (
 <Route path="/" element={<Navigate to="/calc" replace />} />
 <Route path="/calc" element={<DeckAreaCalculator />} />
 <Route path="/start" element={<StartForm />} />
+<Route path="/gracias" element={<ThankYou />} />
 <Route path="*" element={<Navigate to="/calc" replace />} />
 </Routes>
 );
